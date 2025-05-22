f2a87d36e11c8556ca06b4eeb7eefc25f5d313e5
bobOps <robbieirungu82@gmail.com>
2025-05-22 21:58:39 +0300

    set up development module

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,5 +2,6 @@ const express = require('express') //require in the package
 const app = express()  //create backend app
 const PORT = 8383
 
+
 app.listen(PORT, ()=> console.log(`Server has started on: ${PORT}`))
 
