eec0d57a8a25818838d0192468b6f4f851446539
bobOps <robbieirungu82@gmail.com>
2025-06-03 22:09:13 +0300

    set middleware to serve static files

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
-console.log('hey bobops')
-app.listen(PORT, ()=>{
+//Get the file path from the URL of the current module
+
+const __filename = fileURLToPath(import.meta.url)
+//Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+
+//Serves the HTML file from the public directory
+//Tells express to serve all files from the public folder 
+// as static assets/file. Any requests for the css files will be resolved to the public directory
+
+
+//Serving upthe Html file from the /public directory
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname,'public' ,'index.html'))
+})
+app.listen(PORT, ()=>{ 
   console.log(`server has started on port: ${PORT}`)
 })
