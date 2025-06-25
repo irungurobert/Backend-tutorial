ffdf4b5d0e06323156f3ad8ddbe2d3c428d107a7
bobOps <robbieirungu82@gmail.com>
2025-06-25 20:36:17 +0300

    create authentication routes and test

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -8,15 +8,73 @@ const router = express.Router()
  router.post('/register',(req, res)=>{
   const { username, password } = req.body
 
-  let hashedPassword = bcrypt.hashSync(password, 10)
-   console.log(hashedPassword)
-   res.sendStatus(201)
+  // encrypt the password
+  const hashedPassword = bcrypt.hashSync(password, 10)
+   
+ //New function
+ // save the new user and hashed password to the db
+
+  try {
+    
+    const insertUser = db.prepare(`INSERT INTO users (username, password)
+       VALUES (?, ?)`)
+     const result = insertUser.run(username, hashedPassword)  
+
+     console.log(result.lastInsertRowid)
+
+    //  const selectAllUsers = db.prepare(`SELECT * FROM users`)
+    //  const users = selectAllUsers.all()
+    //  console.log(users)
+      
+     // now that we have a user, I want to add their first todo for them
+     const defaultTodo = `Hello :) Add your first todo!`
+     const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
+       VALUES (?, ?)`)
+    insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+
+      // create a token
+      
+        const token = jwt.sign({id: result.lastInsertRowid},
+           process.env.JWT_SECRET, { expiresIn: '24h' })
+           res.json({ token })
+  } catch (error) {
+    console.log(error.message)
+    res.sendStatus(503)
+  }
 
  }
 )
 
 //Login user
 router.post('/login', (req,res)=>{
+
+  const { username, password } = req.body
+
+  try {
+    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`)
+    const user = getUser.get(username)
+
+    if(!user){return res.status(404).send({ message:
+      'User not found' })}
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password)
+    if(!passwordIsValid){
+      return res.status(401).send({ message: 'password is invalid' })
+    }
+    console.log(user)
+
+
+    
+    const token = jwt.sign({ id:user.id },process.env.JWT_SECRET,
+      { expiresIn:'24h' }
+    )
+
+    res.json({token})
+  } catch (error) {
+    console.log(error.message)
+    res.sendStatus(503)
+  }
 })
 
 export default router;
