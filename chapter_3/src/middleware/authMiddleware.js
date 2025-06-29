6eb0d076005f276caf6979e5047e0e804a83b308
bobOps <robbieirungu82@gmail.com>
2025-06-29 15:54:30 +0300

    set authentication middleware and test in a todo crud

--- chapter_3/src/middleware/authMiddleware.js
+++ chapter_3/src/middleware/authMiddleware.js
@@ -1,1 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+
 //handle all the authentication between client-server side and the backend -frontend
+
+function authMiddleware(req,res,next){
+  const token = req.headers['authorization']
+
+  if(!token){
+    return res.status(401).json({message:'no token provided'})
+  }
+
+  jwt.verify(token,process.env.JWT_SECRET, (err,decoded)=>{
+      
+    if(err){
+     return res.status(401).json({message:'invalid token'})
+    }
+
+    req.userId = decoded.id
+
+    next()
+  })
+}
